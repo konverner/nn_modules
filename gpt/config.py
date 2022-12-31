0fd348cef2e067c21b247b7449f4c71420f8e2dc
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2022-12-31 23:07:09 +0100

    Create config.py

---
+++ gpt/config.py
@@ -0,0 +1,14 @@
+# model config
+EMB_DIM = 16
+MAX_LENGTH = 12
+N_LAYERS = 4
+N_HEADS = 2
+DROPOUT = 0.1
+
+# training config
+WEIGHT_DECAY = 0.001
+BATCH_SIZE = 32
+N_EPOCHS = 100
+
+# paths
+DATA_PATH = 'names.txt'
