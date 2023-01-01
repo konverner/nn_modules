6cdb8ca9dab55bf8fa7b296008b81b2bab2171db
Konstantin Verner <52051213+konverner@users.noreply.github.com>
2023-01-02 00:05:37 +0100

    Update config.py

--- gpt/config.py
+++ gpt/config.py
@@ -1,5 +1,5 @@
 # model config
-EMB_DIM = 16
+EMB_DIM = 32
 MAX_LENGTH = 12
 N_LAYERS = 4
 N_HEADS = 2
@@ -8,12 +8,20 @@ DROPOUT = 0.1
 # training config
 WEIGHT_DECAY = 0.001
 BATCH_SIZE = 32
-N_EPOCHS = 2000
-LR = 0.00001
+N_EPOCHS = 3000
+LR = 0.0001
 WEIGHT_DECAY = 0.
 LOG_EVERY_N = 500
+DEVICE = 'cpu'
 
-# paths
+# training paths
 DATA_PATH = 'names.txt'
 SAVE_MODEL_PATH = ''
-LOAD_MODEL_PATH = ''
+
+# generation config
+NUMB_SAMPLES = 5  # a number of samples to generate
+K_CANDIDATES = 3  # a number of token candidates to take while generating a sequence
+
+# generation paths
+LOAD_MODEL_PATH = ""  # path to checkpoint (e.g. checkpoint_0.832.pt)
+VOCAB_PATH = 'vocab.json'
